a71d61b19c11d8ae95084e779e198dd639f99839
bhargavchauhan1968 <43293652+bhargavchauhan1968@users.noreply.github.com>
2020-08-11 22:26:36 +0530

    beta v0.1.0

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(window).on("load", function(){
-    $(".loader .inner").fadeToggle(750, function(){
-        $(".loader").fadeOut(1000);
-    });
+    setTimeout(function(){
+        $(".loader .inner").fadeOut(500, function(){
+            $(".loader").fadeOut(300,"linear");
+        });
+    }, 500)
 });
-
 
 
 $(function() {
@@ -43,7 +44,10 @@ $(function() {
 
 
     var skillsTopOffset = $(".skills").offset().top;
+    console.log(skillsTopOffset);
+    
     $(window).scroll(function(){
+        console.log(window.pageYOffset);
     	if(window.pageYOffset > skillsTopOffset - $(window).height() + 300){
     		$('.chart').easyPieChart({
                 easing: 'easeInOut',
@@ -60,6 +64,54 @@ $(function() {
             });
     	}
     });
+
+
+    const projectItems = document.querySelector(".all-projects");
+    const modal = document.querySelector(".modal")
+    const modalCloseBtn = modal.querySelector(".modal-close-btn");
+    const modalCloseIcon = modal.querySelector(".close");
+
+    projectItems.addEventListener("click",function(event){
+    if(event.target.tagName.toLowerCase() == "button"){
+        const item =event.target.parentElement;
+        const h1 = item.querySelector("h1").innerHTML;
+        const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+        console.log(readMoreCont)
+        modal.querySelector("h1").innerHTML = h1;
+        modal.querySelector(".modal-body").innerHTML = readMoreCont;
+        popupBox();
+        toggle();
+        // $('body').css('overflow','hidden')
+    }
+
+    })
+
+    modalCloseBtn.addEventListener("click", popupBox);
+    modalCloseIcon.addEventListener("click", popupBox);
+    modalCloseBtn.addEventListener("click", toggle);
+    modalCloseIcon.addEventListener("click", toggle);
+
+    modal.addEventListener("click", function(event){
+        if(event.target == modal){
+        popupBox();
+        toggle();
+        // $('body').css('overflow','auto')
+        }
+    })
+
+    function popupBox(){
+        modal.classList.toggle("open");
+        // $('body').css('overflow','auto')
+    }
+
+    function toggle(){
+        var blur = document.getElementsByClassName('blur');
+        console.log(blur);
+        for (i = 0; i < blur.length; i++) {
+            blur[i].classList.toggle('active');
+        }
+        
+    }
 });
 
 
@@ -89,49 +141,49 @@ $(function() {
 // }
 
 
-const projectItems = document.querySelector(".all-projects");
-const modal = document.querySelector(".modal")
-const modalCloseBtn = modal.querySelector(".modal-close-btn");
-const modalCloseIcon = modal.querySelector(".close");
+// const projectItems = document.querySelector(".all-projects");
+// const modal = document.querySelector(".modal")
+// const modalCloseBtn = modal.querySelector(".modal-close-btn");
+// const modalCloseIcon = modal.querySelector(".close");
 
-projectItems.addEventListener("click",function(event){
-if(event.target.tagName.toLowerCase() == "button"){
-    const item =event.target.parentElement;
-    const h1 = item.querySelector("h1").innerHTML;
-    const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
-    console.log(readMoreCont)
-    modal.querySelector("h1").innerHTML = h1;
-    modal.querySelector(".modal-body").innerHTML = readMoreCont;
-    popupBox();
-    toggle();
-    // $('body').css('overflow','hidden')
-}
+// projectItems.addEventListener("click",function(event){
+// if(event.target.tagName.toLowerCase() == "button"){
+//     const item =event.target.parentElement;
+//     const h1 = item.querySelector("h1").innerHTML;
+//     const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+//     console.log(readMoreCont)
+//     modal.querySelector("h1").innerHTML = h1;
+//     modal.querySelector(".modal-body").innerHTML = readMoreCont;
+//     popupBox();
+//     toggle();
+//     // $('body').css('overflow','hidden')
+// }
 
-})
+// })
 
-modalCloseBtn.addEventListener("click", popupBox);
-modalCloseIcon.addEventListener("click", popupBox);
-modalCloseBtn.addEventListener("click", toggle);
-modalCloseIcon.addEventListener("click", toggle);
+// modalCloseBtn.addEventListener("click", popupBox);
+// modalCloseIcon.addEventListener("click", popupBox);
+// modalCloseBtn.addEventListener("click", toggle);
+// modalCloseIcon.addEventListener("click", toggle);
 
-modal.addEventListener("click", function(event){
-    if(event.target == modal){
-    popupBox();
-    toggle();
-    // $('body').css('overflow','auto')
-    }
-})
+// modal.addEventListener("click", function(event){
+//     if(event.target == modal){
+//     popupBox();
+//     toggle();
+//     // $('body').css('overflow','auto')
+//     }
+// })
 
-function popupBox(){
-    modal.classList.toggle("open");
-    // $('body').css('overflow','auto')
-}
+// function popupBox(){
+//     modal.classList.toggle("open");
+//     // $('body').css('overflow','auto')
+// }
 
-function toggle(){
-    var blur = document.getElementsByClassName('blur');
-    console.log(blur);
-    for (i = 0; i < blur.length; i++) {
-        blur[i].classList.toggle('active');
-      }
+// function toggle(){
+//     var blur = document.getElementsByClassName('blur');
+//     console.log(blur);
+//     for (i = 0; i < blur.length; i++) {
+//         blur[i].classList.toggle('active');
+//       }
     
-}
+// }
